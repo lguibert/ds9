b21a08434fa4ace67ba247c2b23dbf875a150a2f
lguibert <lucas.guibert@viacesi.fr>
2014-10-05 18:16:55 -0700

    security for delete & update

--- ds9s/urls.py
+++ ds9s/urls.py
@@ -3,8 +3,6 @@ from ds9s.views import ViewHome, DeleteUser
 
 urlpatterns = patterns('ds9s.views',
 	url(r'^/?$', ViewHome.as_view(), name="ds9s_home"),
-	url(r'^/date$','tpl'),
-	url(r'^/add/(?P<num1>[0-9]+)/(?P<num2>[0-9]+)/$','addition'),
 	url(r'^/view/(?P<id>[0-9]+)','focus'),
 	url(r'^/login/?$','connect'),
 	url(r'^/logout/?$','deconnect'),
--- ds9s/views.py
+++ ds9s/views.py
@@ -1,15 +1,15 @@
 #-*- coding: utf-8 -*-
 from django.http import HttpResponse, HttpResponseRedirect
-from datetime import datetime
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
-from ds9s.forms import ConnectForm, CreateUserForm, UpdateUserForm, UpdateUserForm
+from ds9s.forms import ConnectForm, CreateUserForm, UpdateUserForm
 from django.db import IntegrityError
 from django.views.generic import TemplateView, ListView, DeleteView, UpdateView
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
+from django.utils.decorators import method_decorator
 
 @login_required
 def focus(request, id):
@@ -20,14 +20,6 @@ def focus(request, id):
 	else:
 		user = get_object_or_404(User, id=id)
 		return render(request, 'focus.html', {'user': user})
-
-
-def tpl(request):
-	return render(request, 'tpl.html', {'current_date': datetime.now()})
-
-def addition(request, num1, num2):
-	total = int(num1) + int(num2)
-	return render(request, 'add.html', {'total': total})
 
 def newUser(request):
 	if request.method == "POST":
@@ -53,6 +45,7 @@ def newUser(request):
 	return render(request, 'newUser.html',locals())
 
 @login_required
+@permission_required('ds9s.update_user')
 def updateUser(request, pk):
 	formType = True #change the <form> in the template
 	data = User.objects.get(id=pk)
@@ -121,9 +114,15 @@ class ViewHome(ListView):
 	#paginate_by = 3
 	#queryset = Users.objects.filter(role_id=1) #{can add some filter with queryset}
 
+
 class DeleteUser(DeleteView):
 	model = User
 	context_object_name = "u"
 	template_name = "delete.html"
 	success_url = "/ds9s/"
+
+	@method_decorator(login_required)
+	@method_decorator(permission_required('ds9s.user_delete',login_url='/ds9s/'))
+	def dispatch(self, *args, **kwargs):
+		return super(DeleteUser, self).dispatch(*args, **kwargs)
 
