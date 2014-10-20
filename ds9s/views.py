daa28c480b72a8ecc5ddcc518f6c635ffea6a33d
root <root@localhost.localdomain>
2014-10-19 22:43:26 -0700

    delete all

--- ds9s/views.py
+++
@@ -1,6 +0,0 @@
-#-*- coding: utf-8 -*-
-
-#I break this file in several other. One view file per type of use.
-#see :
-# For user : views/user_views.py 
-# For fits : views/fits_views.py
--- ds9s/views/__init__.py
+++
@@ -1,3 +0,0 @@
-#-*- coding: utf-8 -*-
-from user_views import *
-from fits_views import *
